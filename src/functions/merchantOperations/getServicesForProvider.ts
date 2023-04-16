b0bbcf30579bec7f67eb80a9a7af686e795d0f3e
BrightonMboya <b.mboya@alustudent.com>
2023-04-16 18:49:37 +0200

    added the getServicesForProvider.ts

--- src/functions/merchantOperations/getServicesForProvider.ts
+++ src/functions/merchantOperations/getServicesForProvider.ts
@@ -1,20 +1,17 @@
 import axios from "axios";
 import convert from "xml-js";
+import { type serviceType } from "./getServices";
 
-export type serviceType = {
-    serviceID: string,
-    ServiceName: string,
-}
+const errorCodes = ["801", "802", "803", "804", "902", "950"]
 
-const errorCodes = ["801", "802", "803", "804"]
-// console.log(errorCodes.includes(801))
 
-export default async function getServices(companyToken: string) {
+export default async function getServicesForProvider(companyToken: string, providerToken: string) {
     const data = `
     <?xml version="1.0" encoding="utf-8"?>
     <API3G>
-    <Request>getServices</Request>
+    <Request>getServicesforprovider</Request>
     <CompanyToken>${companyToken}</CompanyToken>
+    <ProviderID>${providerToken}</ProviderID>
     </API3G>
     `;
     let config = {
@@ -29,32 +26,28 @@ export default async function getServices(companyToken: string) {
     try {
         const xmlResponse = await axios.request(config);
         const jsonResponse = convert.xml2js(xmlResponse.data, { compact: true, alwaysChildren: true });
-
         if (errorCodes.includes(jsonResponse["API3G"]["Result"]["_text"])) {
             const parsedJson = {
                 Result: jsonResponse["API3G"]["Result"]["_text"],
                 ResultExplanation: jsonResponse["API3G"]["ResultExplanation"]["_text"],
             }
-
             return parsedJson;
         } else {
-
             const parsedJson = {
                 Services: jsonResponse["API3G"]["Services"]["Service"].map((service: serviceType) => {
                     return {
                         serviceID: service["ServiceID"]["_text"],
                         ServiceName: service["ServiceName"]["_text"],
-
                     }
                 })
             }
             return parsedJson;
         }
-    } catch (error) {
+    }
+    catch (error) {
         console.log(error);
         return error;
     }
 }
-
-const res = await getServices("0B6758B3-BB98-438A-A666-7BF2F9CA8B31")
+const res = await getServicesForProvider("0B6758B3-BB98-438A-A666-7BF2F9CA8B31", "CA55CE70-770B-4DFC-BBD0-D02117914C5D")
 console.log(res)
